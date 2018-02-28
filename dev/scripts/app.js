3b3e62ee43d4033a9607959425fd9046c26734b2
Manon Rugge <manonrugge@gmail.com>
2018-02-28 15:15:33 -0500

    add styles from the styleguide

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -12,7 +12,9 @@ class App extends React.Component {
     render() {
       return (
         <div>
-         Hello
+          <img src="./dev/images/brandit-logo.svg" alt=""/>
+         <h2>Brandit</h2>
+         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta tenetur necessitatibus, explicabo doloremque ab eos dolorem id velit obcaecati optio saepe reprehenderit facere provident nisi quos alias harum illum distinctio?</p>
         </div>
       )
     }
